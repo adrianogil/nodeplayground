e94fb5fbd30e85969e97b3e80f53124b6999f21b
Adriano Gil <adrianogil.san@gmail.com>
2023-02-20 15:59:08 -0400

    [CompleteNodeJS] Section04 - Lesson 39 - using get from https module

--- udemy/completenodejs2023/section04/39_http_requests/main.js
+++ udemy/completenodejs2023/section04/39_http_requests/main.js
@@ -1,6 +1,6 @@
-const http = require('http');
+const { get } = require('https');
 
-const req = http.request('http://www.google.com', (res) => {
+get('https://www.google.com', (res) => {
     res.on('data', (chunk) => { 
         console.log(`Data chunk: ${chunk}`);
     });
@@ -8,4 +8,3 @@ const req = http.request('http://www.google.com', (res) => {
         console.log('No more data');
     });
 });
-req.end()

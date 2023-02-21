6fb9710790e483d5f7d1b39ceee4a81722a825be
Adriano Gil <adrianogil.san@gmail.com>
2023-02-20 20:14:38 -0400

    [CompleteNodeJS] Section04 - Lesson 46

---
+++ udemy/completenodejs2023/section04/46_index/https.js
@@ -0,0 +1,13 @@
+const internals = require('./internals')
+// const request = require('./request');
+// const response = require('./response');
+
+function makeRequest(url, data) {
+    internals.request.send(url, data);
+    return internals.response.read();
+}
+
+const responseData = makeRequest('https://www.google.com', 'my_data');
+console.log(responseData)
+
+// console.log(require.cache)

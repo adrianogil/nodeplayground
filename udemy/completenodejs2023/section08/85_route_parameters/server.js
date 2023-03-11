008ab5c2c6fb899c9418f20a18c5a659a3024c31
Adriano Gil <adrianogil.san@gmail.com>
2023-03-11 15:48:02 -0400

    [CompleteNodeJS] Section07 - Lesson 86 - route parameters

---
+++ udemy/completenodejs2023/section08/85_route_parameters/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [
+    {
+        id: 0,
+        name: 'Isaac Newton'
+    },
+    {
+        id: 1,
+        name: 'Albert Einsten'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Heeeellloooo');
+})
+
+app.get('/messages', (req, res) => {
+    res.send('<ul><li>Hello from Moi</li><ul>');
+})
+
+app.get('/friends', (req, res) => {
+    res.send(friends);
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = req.params.friendId;
+    const friend = friends[Number(friendId)];
+
+    if (friend) {
+        res.json(friend);
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        })
+    };
+        
+})
+
+app.post('/messages', (req, res) => {
+    console.log('Updating messages...');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})

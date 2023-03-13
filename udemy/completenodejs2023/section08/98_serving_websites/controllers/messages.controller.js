5b9996b2861bf9543e0306ba7770a7370e9144e0
Adriano Gil <adrianogil.san@gmail.com>
2023-03-12 20:29:45 -0400

    [CompleteNodeJS] Section08 - Lesson 98 - serving websites

---
+++ udemy/completenodejs2023/section08/98_serving_websites/controllers/messages.controller.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+
+const messages = [
+    "Hello from Moi"
+]
+
+function getMessages(req, res) {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<ul>');
+    for (let index = 0; index < messages.length; index++) {
+        const message = messages[index];
+        res.write(`<li>${message}</li>`);
+    }
+    res.write('<ul>');
+    res.end();
+}
+
+function getMessageImage(req, res) {
+    const filePath = path.join(global.appRoot, 'public', 'images', 'skimountain.jpg');
+    console.log(`Sending file ${filePath}`)
+    res.sendFile(filePath);
+}
+
+function postMessages(req, res) {
+    if (!req.body.message) {
+        return res.status(400).json({
+            error: 'Missing message field'
+        })
+    }
+
+    console.log('Updating messages...');
+    const newMessage = req.body.message
+    messages.push(newMessage)
+    res.json({"message": newMessage})
+}
+
+module.exports = {
+    getMessages,
+    postMessages,
+    getMessageImage
+}

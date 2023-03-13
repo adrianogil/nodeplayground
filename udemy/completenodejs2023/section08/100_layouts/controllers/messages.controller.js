238fae87787f1281d613efcfc758b4618ab83b4a
Adriano Gil <adrianogil.san@gmail.com>
2023-03-12 21:00:15 -0400

    [CompleteNodeJS] Section08 - Lesson 100 - Layout

---
+++ udemy/completenodejs2023/section08/100_layouts/controllers/messages.controller.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+
+const messages = [
+    "Hello from Moi"
+]
+
+function getMessages(req, res) {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<ul>');
+    for (let index = 0; index < messages.length; index++) {
+        const message = messages[index];
+        res.write(`<li>${message}</li>`);
+    }
+    res.write('<ul>');
+    res.end();
+}
+
+function renderFriendMessage(req, res) {
+    res.render('messages', {
+        title: 'Messages to my Friends!',
+        friend: 'Jonh Carmark'
+    })
+}
+
+function getMessageImage(req, res) {
+    const filePath = path.join(global.appRoot, 'public', 'images', 'skimountain.jpg');
+    console.log(`Sending file ${filePath}`)
+    res.sendFile(filePath);
+}
+
+function postMessages(req, res) {
+    if (!req.body.message) {
+        return res.status(400).json({
+            error: 'Missing message field'
+        })
+    }
+
+    console.log('Updating messages...');
+    const newMessage = req.body.message
+    messages.push(newMessage)
+    res.json({"message": newMessage})
+}
+
+module.exports = {
+    getMessages,
+    postMessages,
+    getMessageImage,
+    renderFriendMessage
+}
---
+++ udemy/completenodejs2023/section08/100_layouts/models/friends.model.js
@@ -0,0 +1,14 @@
+const friends = [
+    {
+        id: 0,
+        name: 'Isaac Newton'
+    },
+    {
+        id: 1,
+        name: 'Albert Einsten'
+    }
+]
+
+module.exports = {
+    friends
+};
